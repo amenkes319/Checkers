22d31d19527f9f609ec2c2f68002b0fcbe1f2937
amenkes319 <amenkes319@gmail.com>
2022-04-23 02:11:15 -0400

    Restructure to use 8x8 board and simplify the Pieces

--- Board.cpp
+++ Board.cpp
@@ -4,30 +4,85 @@ Board::Board() {
 	ResetBoard();
 }
 
-void Board::ResetBoard() {
-	DeleteBoard();
+bool Board::Move(Coords piece, Coords target) {
+	if (IsValidMove(piece, target)) {
+		m_board[target.row][target.col] = m_board[piece.row][piece.col];
+		m_board[piece.row][piece.col] = EMPTY;
+		return true;
+	}
+	return false;
+}
 
+void Board::ResetBoard() {
 	for (int i = 0; i < BOARD_SIZE; i++) {
-		for (int j = 0; j < BOARD_SIZE / 2; j++) {
-			if (i < 3) {
-				board[i][j] = new Piece(PieceColor::BLACK);
-			} else if (i < 5) {
-				board[i][j] = nullptr;
+		for (int j = 0; j < BOARD_SIZE; j++) {
+			if (i < 3 && j % 2 != i % 2) {
+				m_board[i][j] = BLACK_PIECE;
+			} else if (i >= 5 && j % 2 != i % 2) {
+				m_board[i][j] = RED_PIECE;
 			} else {
-				board[i][j] = new Piece(PieceColor::RED);
+				m_board[i][j] = EMPTY;
 			}
 		}
 	}
 }
 
-void Board::DeleteBoard() {
-	for (int i = 0; i < BOARD_SIZE; i++) {
-		for (int j = 0; j < BOARD_SIZE / 2; j++) {
-			delete board[i][j];
-		}
-	}
+bool Board::IsValidMove(Coords piece, Coords target) {
+	std::vector<Coords> moves = PossibleMoves(piece);
+	return true;
+	//return std::find(moves.begin(), moves.end(), target) != moves.end();
 }
 
-Board::~Board() {
-	DeleteBoard();
+/**
+ * Helper function to determine if an index is within the bounds of the board
+ * @param index Index to test
+ * @return true if index is between [0, BOARD_SIZE)
+ */
+static bool IsValid(int index) {
+	return index >= 0 && index < BOARD_SIZE;
+}
+
+// TODO: Find possible jumps
+std::vector<Coords> Board::PossibleMoves(Coords piece) {
+	std::vector<Coords> moves;
+	Coords c;
+	for (int i = -1; i <= 1; i += 2) {
+		for (int j = -1; j <= 1; j += 2) {
+			if (IsValid(piece.row + i) && IsValid(piece.col + j)) {
+				c = { piece.row + i, piece.col + j };
+				moves.push_back(c);
+			}
+		}
+	}
+
+	return moves;
+}
+
+// TODO: Implement backtracking to simulate possible game moves and look ahead
+std::vector<Board> Board::LookAhead() {
+	std::vector<Board> boards;
+
+	return boards;
+}
+
+void Board::PrintBoard() {
+	for (int i = 0; i < BOARD_SIZE; i++) {
+		for (int j = 0; j < BOARD_SIZE; j++) {
+			char p = '-';
+			switch (m_board[i][j]) {
+			case 1:
+			case 2:
+				p = 'X';
+				break;
+			case -1:
+			case -2:
+				p = 'O';
+				break;
+			}
+
+			std::cout << p;
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
 }
--- Board.h
+++ Board.h
@@ -3,28 +3,68 @@
 #ifndef BOARD_H
 #define BOARD_H
 
-#include "Piece.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
 #define BOARD_SIZE 8
 
+/**
+ * Enumeration to represent the possible states of a square of the board
+ * Black pieces are positive, reds are negative
+ */
+enum Piece {
+	EMPTY       = 0,
+	BLACK_PIECE = 1,
+	BLACK_KING  = 2,
+	RED_PIECE   = -1,
+	RED_KING    = -2,
+};
+
+struct Coords {
+	int row;
+	int col;
+};
+
+/* Represent a configuration of a checkers board */
 class Board
 {
 public:
 	Board();
 
 	/**
-	 * Delete current memory allocated to piece pointers (if any) and create
-	 * new ones at the original positions
+	 * Move piece to target coordinates
+	 * @param piece Coordinates of desired piece to move
+	 * @param target Coordinates of the target to move the piece to
+	 * @return true if the move is valid
+	 */
+	bool Move(Coords piece, Coords target);
+
+	/**
+	 * Reset positions of pieces
 	 */
 	void ResetBoard();
 
 	/**
-	 * Deletes the memory allocated to the board pieces
+	 * Finds all possible moves that can be made by the piece
+	 * @param piece Coordinates of desired piece to move
+	 * @return vector of all possible coordinates the piece can move to
 	 */
-	void DeleteBoard();
-	~Board();
+	std::vector<Coords> PossibleMoves(Coords piece);
+
+	std::vector<Board> LookAhead();
+
+	void PrintBoard();
 
 private:
-	Piece* board[BOARD_SIZE][BOARD_SIZE / 2];
+	/**
+	 * Returns if the coords of the piece can move to the target
+	 * @param piece Coordinates of desired piece to move
+	 * @param target Coordinates of the target to move the piece to
+	 * @return true if the piece can move to the target
+	 */
+	bool IsValidMove(Coords piece, Coords target);
+
+	Piece m_board[BOARD_SIZE][BOARD_SIZE];
 };
 
 #endif
